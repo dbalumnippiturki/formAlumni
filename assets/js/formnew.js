0cf8ddfa8b20d7b463ab33a6d90f75ec82e157d6
Haries Ramdhani <hydrolizedmaltose@gmail.com>
2017-04-04 15:24:44 +0300

    Form almost finished

--- assets/js/formnew.js
+++ assets/js/formnew.js
@@ -2,11 +2,27 @@ $('form select[name=kegiatan]').change(function(e){
   if ($('form select[name=kegiatan]').val() == '1'){
     $('#lanjutKuliah').show();
 		$('#bekerja').hide();
+		$('#entrepre').hide();
+		$('#klainnya').hide();
   } else if ($('form select[name=kegiatan]').val() == '2'){
     $('#bekerja').show();
 		$('#lanjutKuliah').hide();
+		$('#entrepre').hide();
+		$('#klainnya').hide();
+	} else if ($('form select[name=kegiatan]').val() == '3'){
+    $('#entrepre').show();
+		$('#bekerja').hide();
+		$('#lanjutKuliah').hide();
+		$('#klainnya').hide();
+	} else if ($('form select[name=kegiatan]').val() == '4'){
+    $('#klainnya').show();
+		$('#bekerja').hide();
+		$('#lanjutKuliah').hide();
+		$('#entrepre').hide();
   } else {
 		$('#lanjutKuliah').hide();
 		$('#bekerja').hide();
+		$('#entrepre').hide();
+		$('#klainnya').hide();
 	}
 });
